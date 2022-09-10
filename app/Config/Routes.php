802d99e4509155b10ab444969455b92f7e104749
Digamber Rawat <digamber@positronx.io>
2022-09-10 05:44:22 +0530

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // Route since we don't have to scan directories.
 
 $routes->get('/', 'UploadMultipleFiles::index');
+$routes->match(['get', 'post'], 'UploadMultipleFiles/uploadFiles', 'UploadMultipleFiles::uploadFiles');
 
 /**
  * --------------------------------------------------------------------
